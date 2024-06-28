06d2ed29694223649fae5bf0102db3298052523e
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-06-28 12:33:25 +1000

    Add files via upload

---
+++ tools_scripts/Multigrate/main_Multigrate_mosaic_rna_atac.py
@@ -0,0 +1,150 @@
+import os
+import time
+import h5py
+import muon
+import random
+import argparse
+import numpy as np
+import scanpy as sc
+import pandas as pd
+import anndata as ad
+import multigrate as mtg
+import scipy.sparse as sp
+from scipy.sparse import csr_matrix
+
+random.seed(1)
+parser = argparse.ArgumentParser("Multigrate")
+parser.add_argument('--path1', metavar='DIR', default="", help='path to train data1')
+parser.add_argument('--path2', metavar='DIR', default="", help='path to train data2')
+parser.add_argument('--path3', metavar='DIR', default="", help='path to train data2')
+parser.add_argument('--path4', metavar='DIR', default="", help='path to train data2')
+parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
+parser.add_argument('--epochs', type = int, default=200, help='')
+parser.add_argument('--lr', type = float, default=1e-3, help='')
+
+args = parser.parse_args()
+
+begin_time = time.time()
+
+
+def process_rna(adata_rna,barcodes,featurs):
+    adata_rna.layers['counts'] = adata_rna.X
+    adata_rna.obs.index = barcodes
+    adata_rna.var_names = featurs
+    sc.pp.normalize_total(adata_rna, target_sum=1e4)
+    sc.pp.log1p(adata_rna)
+    return adata_rna
+    
+def process_atac(adata_atac,barcodes,featurs):
+    adata_atac.layers['counts'] = adata_atac.X
+    adata_atac.obs.index = barcodes
+    adata_atac.var_names = featurs
+    sc.pp.normalize_total(adata_atac, target_sum=20000)
+    sc.pp.log1p(adata_atac)
+    adata_atac.layers['log-norm'] = adata_atac.X.copy()
+    return adata_atac
+
+def process_adt(adata_adt,barcodes,featurs):
+    adata_adt.layers['counts'] = adata_adt.X
+    adata_adt.obs.index = barcodes
+    adata_adt.var_names = featurs
+    muon.prot.pp.clr(adata_adt)
+    return adata_adt
+    
+
+def h5_to_matrix(path):
+    with h5py.File(path, "r") as f:
+        X = csr_matrix(np.mat(np.array(f['matrix/data']).transpose()))
+        barcodes = [key.decode('UTF-8') for key in f['matrix/barcodes']]
+        features = [key.decode('UTF-8') for key in f['matrix/features']]
+    return X, barcodes, features
+    
+    
+rna_path1 = args.path1
+rna_path2 = args.path2
+adt_path2 = args.path3
+adt_path3 = args.path4
+
+rna1, barcodes1, rna_features = h5_to_matrix(rna_path1)
+rna2, barcodes2, rna_features = h5_to_matrix(rna_path2)
+adt2, barcodes2, adt_features = h5_to_matrix(adt_path2)
+adt3, barcodes3, adt_features = h5_to_matrix(adt_path3)
+
+
+adata_rna1 = process_rna(ad.AnnData(rna1),barcodes1, rna_features)
+adata_rna2 = process_rna(ad.AnnData(rna2),barcodes2, rna_features)
+adata_adt2 = process_atac(ad.AnnData(adt2), barcodes2, adt_features)
+adata_adt3 = process_atac(ad.AnnData(adt3), barcodes3, adt_features)
+
+adata_rna1.obs['Modality'] = "rna"
+adata_rna2.obs['Modality'] = "cite"
+adata_adt2.obs['Modality'] = "cite"
+adata_adt3.obs['Modality'] = "adt"
+
+adata_rna = ad.concat([adata_rna1, adata_rna2], merge='same')
+sc.pp.highly_variable_genes(adata_rna, n_top_genes=4000,  batch_key='Modality')
+adata_rna1 = adata_rna1[:, adata_rna.var.highly_variable]
+adata_rna2 = adata_rna2[:, adata_rna.var.highly_variable]
+
+adata_adt = ad.concat([adata_adt2, adata_adt3], merge='same')
+sc.pp.highly_variable_genes(adata_adt, n_top_genes=2000,  batch_key='Modality')
+adata_adt2 = adata_adt2[:, adata_adt.var.highly_variable]
+adata_adt3 = adata_adt3[:, adata_adt.var.highly_variable]
+
+print(adata_rna2.obs.index == adata_adt2.obs.index)
+indices = range(1, len(adata_rna1.obs_names) + 1)
+adata_rna1.obs.index= [f"{index}rna-{original}" for index, original in zip(indices, adata_rna1.obs_names)]
+indices = range(1, len(adata_rna2.obs_names) + 1)
+adata_rna2.obs.index= [f"{index}cite-{original}" for index, original in zip(indices, adata_rna2.obs_names)]
+indices = range(1, len(adata_adt2.obs_names) + 1)
+adata_adt2.obs.index= [f"{index}cite-{original}" for index, original in zip(indices, adata_adt2.obs_names)]
+indices = range(1, len(adata_adt3.obs_names) + 1)
+adata_adt3.obs.index= [f"{index}adt-{original}" for index, original in zip(indices, adata_adt3.obs_names)]
+
+adata = mtg.data.organize_multiome_anndatas(
+    adatas = [[adata_rna1, adata_rna2, None], [None, adata_adt2, adata_adt3]],
+    layers = [['counts', 'counts', None], [None, 'log-norm', 'log-norm']],
+)
+adata
+
+
+    
+mtg.model.MultiVAE.setup_anndata(
+    adata,
+    categorical_covariate_keys=['Modality'],# 'Samplename'],
+    rna_indices_end=4000
+)
+
+model = mtg.model.MultiVAE(
+    adata,
+    losses=['nb', 'mse'],
+    loss_coefs={'kl': 1e-3,
+               'integ': 4000,
+               },
+    integrate_on='Modality',
+    mmd='marginal',
+)
+
+model.train()
+
+model.get_latent_representation()
+result = adata.obsm['latent']
+
+
+end_time = time.time()
+all_time = end_time - begin_time
+print(all_time)
+print(result.shape)
+
+if not os.path.exists(args.save_path):
+    os.makedirs(args.save_path)
+    print("create path")
+else:
+    print("the path exits")
+
+
+file = h5py.File(args.save_path+"/embedding.h5", 'w')
+file.create_dataset('data', data=result)
+file.close()
+
+np.savetxt(args.save_path+"/time.csv", [all_time], delimiter=",")
---
+++ tools_scripts/Multigrate/main_Multigrate_paired_integration.py
@@ -0,0 +1,297 @@
+import os
+import time
+import h5py
+import muon
+import random
+import argparse
+import numpy as np
+import scanpy as sc
+import pandas as pd
+import anndata as ad
+import multigrate as mtg
+import scipy.sparse as sp
+
+random.seed(1)
+parser = argparse.ArgumentParser("Multigrate")
+parser.add_argument('--path1', metavar='DIR', nargs='+', default=[], help='path to train data1')
+parser.add_argument('--path2', metavar='DIR', nargs='+', default=[], help='path to train data2')
+parser.add_argument('--path3', metavar='DIR', nargs='+', default=[], help='path to train data3')
+parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
+parser.add_argument('--epochs', type = int, default=200, help='')
+parser.add_argument('--lr', type = float, default=1e-3, help='')
+        
+args = parser.parse_args()
+
+begin_time = time.time()
+
+    
+def process_rna(adata_rna):
+    sc.pp.normalize_total(adata_rna, target_sum=1e4)
+    sc.pp.log1p(adata_rna)
+    print(adata_rna)
+    #adata_rna.X = np.array(adata_rna.X)
+    sc.pp.highly_variable_genes(adata_rna, n_top_genes=4000) 
+    return adata_rna
+    
+def process_atac(adata_atac):
+    sc.pp.normalize_total(adata_atac, target_sum=1e4)
+    sc.pp.log1p(adata_atac)
+    #adata_atac.X = np.array(adata_atac.X)
+    sc.pp.highly_variable_genes(adata_atac, n_top_genes=20000) 
+    return adata_atac
+
+def process_adt(adata_adt):
+    muon.prot.pp.clr(adata_adt)
+    return adata_adt
+    
+def h5_to_matrix(path):
+    with h5py.File(path, "r") as f:
+        X = (np.array(f['matrix/data']).transpose())
+        #barcodes = []
+        #for key in f['matrix/barcodes']:
+        #    barcodes.append(key.decode('UTF-8')) 
+        #features = []
+        #for key in f['matrix/features']:
+        #    features.append(key.decode('UTF-8'))
+    return X
+
+def read_h5_data(rna_path=None, adt_path=None, atac_path=None, list_len=3):
+    rna_list = []
+    adt_list = []
+    atac_list = []
+    
+    # read rna
+    if rna_path is not None:
+        for i in range(len(rna_path)):
+            if rna_path[i] is None:
+                rna_list.append(None)
+            else:
+                rna_list.append(h5_to_matrix(rna_path[i]))
+        
+    # read adt
+    if adt_path is not None:
+        for i in range(len(adt_path)):
+            if adt_path[i] is None:
+                adt_list.append(None)
+            else:
+                adt_list.append(h5_to_matrix(adt_path[i]))
+    
+    # read atac
+    if atac_path is not None:
+        for i in range(len(atac_path)):
+            if atac_path[i] is None:
+                atac_list.append(None)
+            else:
+                atac_list.append(h5_to_matrix(atac_path[i]))
+                
+    print(adt_list)
+    print(atac_list)
+    
+    batch = []
+    if rna_path is not None:
+        for i in range(len(rna_list)):
+            batch.append(np.ones((rna_list[i].shape[0], 1))  + i)
+    elif adt_path is not None:
+        for i in range(len(adt_list)):
+            batch.append(np.ones((adt_list[i].shape[0], 1))  + i)
+    elif atac_path is not None:
+        for i in range(len(atac_list)):
+            batch.append(np.ones((atac_list[i].shape[0], 1))  + i)
+        
+    if (rna_path is not None) and (adt_path is not None) and (atac_path is not None):
+        adata_rna = process_rna(ad.AnnData(np.concatenate(rna_list, axis=0)))
+        adata_adt = process_adt(ad.AnnData(np.concatenate(adt_list, axis=0)))
+        adata_atac = process_atac(ad.AnnData(np.concatenate(atac_list, axis=0)))
+        result = {"rna": adata_rna, "adt": adata_adt, "atac": adata_atac}
+        
+    if (rna_path is not None) and (adt_path is not None) and (atac_path is None):
+        adata_rna = process_rna(ad.AnnData(np.concatenate(rna_list, axis=0)))
+        adata_adt = process_adt(ad.AnnData(np.concatenate(adt_list, axis=0)))
+        result = {"rna": adata_rna, "adt": adata_adt}
+        
+    if (rna_path is not None) and (adt_path is None) and (atac_path is not None):
+        adata_rna = process_rna(ad.AnnData(np.concatenate(rna_list, axis=0)))
+        adata_atac = process_atac(ad.AnnData(np.concatenate(atac_list, axis=0)))
+        result = {"rna": adata_rna, "atac": adata_atac}
+        
+    if (rna_path is None) and (adt_path is not None) and (atac_path is not None):
+        adata_adt = process_adt(ad.AnnData(np.concatenate(adt_list, axis=0)))
+        adata_atac = process_atac(ad.AnnData(np.concatenate(atac_list, axis=0)))
+        result = {"adt": adata_adt, "atac": adata_atac}
+        
+    return result, np.concatenate(batch,0)
+
+
+def run_Multigrate(file_paths):
+    if (file_paths['rna_path'] is not None) and (file_paths['adt_path'] is not None) and (file_paths['atac_path'] is None):
+        result, batch = read_h5_data(file_paths["rna_path"],file_paths["adt_path"],file_paths['atac_path'])
+        result_list = [] 
+        for i in range(len(list(result))):
+            temp = []
+            temp.append(result[list(result.keys())[i]])
+            result_list.append(temp)
+        adata = mtg.data.organize_multiome_anndatas(
+            adatas = result_list, 
+        )
+        adata.obs["Batch"] = batch
+        mtg.model.MultiVAE.setup_anndata(
+            adata,
+            categorical_covariate_keys=['Batch'],
+            rna_indices_end=4000,
+        )
+        model = mtg.model.MultiVAE(
+            adata, 
+            losses=['nb', 'mse'],
+            n_layers_encoders=[2, 2],
+            n_layers_decoders=[2, 2],
+        )
+        model.train(max_epochs=args.epochs, lr=args.lr, batch_size=256)
+        model.get_latent_representation()
+        latent_space = adata.obsm['latent']
+        latent_space1 = latent_space[0:sum(batch==1)[0],:]
+        latent_space2 = latent_space[sum(batch==1)[0]:,:]
+        latent_space_list = [latent_space1, latent_space2]
+
+    elif (file_paths['rna_path'] is None) and (file_paths['adt_path'] is not None) and (file_paths['atac_path'] is not None):
+        result, batch = read_h5_data(file_paths["rna_path"],file_paths["adt_path"],file_paths['atac_path'])
+        result_list = [] 
+        for i in range(len(list(result))):
+            temp = []
+            temp.append(result[list(result.keys())[i]])
+            result_list.append(temp)
+        adata = mtg.data.organize_multiome_anndatas(
+            adatas = result_list, 
+        )
+        adata.obs["Batch"] = batch
+        mtg.model.MultiVAE.setup_anndata(
+            adata,
+            categorical_covariate_keys=['Batch'],
+            rna_indices_end=4000,
+        )
+        model = mtg.model.MultiVAE(
+            adata, 
+            losses=['mse','nb'],
+            loss_coefs={'kl': 1e-5,
+                       'integ': 0,
+                       },
+            n_layers_encoders=[2, 2],
+            n_layers_decoders=[2, 2],
+        )
+        model.train(max_epochs=args.epochs, lr=args.lr, batch_size=256)
+        model.get_latent_representation()
+        latent_space = adata.obsm['latent']
+        latent_space1 = latent_space[0:sum(batch==1)[0],:]
+        latent_space2 = latent_space[sum(batch==1)[0]:,:]
+        latent_space_list = [latent_space1, latent_space2]
+
+    elif (file_paths['rna_path'] is not None) and (file_paths['adt_path'] is None) and (file_paths['atac_path'] is not None):
+        result, batch = read_h5_data(file_paths["rna_path"],file_paths["adt_path"],file_paths['atac_path'])
+        result_list = [] 
+        for i in range(len(list(result))):
+            temp = []
+            temp.append(result[list(result.keys())[i]])
+            result_list.append(temp)
+        adata = mtg.data.organize_multiome_anndatas(
+            adatas = result_list, 
+        )
+        adata.obs["Batch"] = batch
+        mtg.model.MultiVAE.setup_anndata(
+            adata,
+            categorical_covariate_keys=['Batch'],
+            rna_indices_end=4000,
+        )
+        model = mtg.model.MultiVAE(
+            adata, 
+            losses=['nb', 'mse'], #dataset5 nb nb dataset6 nb mse
+            loss_coefs={'kl': 1e-5,
+                       'integ': 0,
+                       },
+            n_layers_encoders=[2, 2],
+            n_layers_decoders=[2, 2],
+        )
+        model.train(max_epochs=args.epochs, lr=args.lr, batch_size=256)
+        model.get_latent_representation()
+        latent_space = adata.obsm['latent']
+        latent_space1 = latent_space[0:sum(batch==1)[0],:]
+        latent_space2 = latent_space[sum(batch==1)[0]:,:]
+        latent_space_list = [latent_space1, latent_space2]
+
+    elif (file_paths['rna_path'] is not None) and (file_paths['adt_path'] is not None) and (file_paths['atac_path'] is not None):
+        result, batch = read_h5_data(file_paths["rna_path"],file_paths["adt_path"],file_paths["atac_path"])
+        result_list = [] 
+        for i in range(len(list(result))):
+            temp = []
+            temp.append(result[list(result.keys())[i]])
+            result_list.append(temp)
+        adata = mtg.data.organize_multiome_anndatas(
+            adatas = result_list, 
+        )
+        adata.obs["Batch"] = batch
+        mtg.model.MultiVAE.setup_anndata(
+            adata,
+            categorical_covariate_keys=['Batch'],
+            rna_indices_end=4000,
+        )
+        model = mtg.model.MultiVAE(
+            adata, 
+            losses=['nb', 'mse', 'mse'],
+            loss_coefs={'kl': 1e-5,
+                       'integ': 0,
+                       },
+            #z_dim = 100,
+            n_layers_encoders=[2, 2, 2],
+            n_layers_decoders=[2, 2, 2],
+        )
+        model.train(max_epochs=args.epochs, lr=args.lr, batch_size=256)
+        model.get_latent_representation()
+        latent_space = adata.obsm['latent']
+        latent_space1 = latent_space[0:sum(batch==1)[0],:]
+        latent_space2 = latent_space[sum(batch==1)[0]:(sum(batch==1)[0]+sum(batch==2)[0]),:]
+        latent_space3 = latent_space[(sum(batch==1)[0]+sum(batch==2)[0]):,:]
+        latent_space_list = [latent_space1, latent_space2, latent_space3]
+    
+    return latent_space_list
+
+# run method
+if args.path2==[]:
+    file_paths = {
+        "rna_path": args.path1,
+        "adt_path": None,
+        "atac_path": args.path3
+    }
+elif args.path3==[]:
+    file_paths = {
+        "rna_path": args.path1,
+        "adt_path": args.path2,
+        "atac_path": None
+    }
+elif args.path1==[]:
+    file_paths = {
+        "rna_path": None,
+        "adt_path": args.path2,
+        "atac_path": args.path3,
+    }
+else:
+    file_paths = {
+        "rna_path": args.path1,
+        "adt_path": args.path2,
+        "atac_path": args.path3
+    }
+result = run_Multigrate(file_paths)
+result = np.concatenate(result,0)
+result = np.transpose(result)
+end_time = time.time()
+all_time = end_time - begin_time
+print(all_time)
+print(result.shape)
+
+# save results
+if not os.path.exists(args.save_path):
+    os.makedirs(args.save_path)
+    print("create path")
+else:
+    print("the path exits")
+file = h5py.File(args.save_path+"/embedding.h5", 'w')
+file.create_dataset('data', data=result)
+file.close()
+np.savetxt(args.save_path+"/time.csv", [all_time], delimiter=",")
