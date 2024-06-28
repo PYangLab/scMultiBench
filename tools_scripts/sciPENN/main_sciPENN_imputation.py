b53fb20cb428edb3628d8230ea7bce0e5d71e4db
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-06-28 12:24:31 +1000

    Add files via upload

---
+++ tools_scripts/sciPENN/main_sciPENN_imputation.py
@@ -0,0 +1,91 @@
+import os
+import h5py
+import time
+import random
+import argparse
+import numpy as np
+import scanpy as sc
+import pandas as pd
+from anndata import AnnData, read_h5ad
+from sciPENN.sciPENN_API import sciPENN_API
+from sciPENN.Preprocessing import preprocess
+
+random.seed(1)
+parser = argparse.ArgumentParser('sciPENN')
+parser.add_argument('--data_path', default='NULL', help='path to load the data')
+parser.add_argument('--train_fids', metavar='trainid', nargs='+', default=[], help='file ids to train data1')
+parser.add_argument('--impute_fids', metavar='imputeid', default='1', help='file ids to train data2')
+parser.add_argument('--save_path', default='NULL', help='path to save the output data')
+args = parser.parse_args()
+print(args)
+
+def data_loader(path, bid):
+    with h5py.File(path, "r") as f:
+        X = np.array(f['matrix/data']).transpose()
+        feat = [i.decode('utf-8') for i in f['matrix/features']]
+        cid = [i.decode('utf-8') for i in f['matrix/barcodes']]
+    adata = AnnData(X=X)
+    adata.obs['bid'] = str(bid)
+    adata.var_names = feat
+    adata.obs_names = cid
+    return adata
+
+
+begin_time = time.time()
+gene_training_list = []
+protein_training_list = []
+
+print("----preparing training data..")
+for trainid in args.train_fids:
+	rna_h5 = os.path.join(args.data_path, 'reference', 'rna'+trainid+'.h5')
+	adt_h5 = os.path.join(args.data_path, 'reference', 'adt'+trainid+'.h5')
+	lb_csv = os.path.join(args.data_path, 'reference', 'cty'+trainid+'.csv')
+	lbs = pd.read_csv(lb_csv)['x'].tolist()
+	print("->Loading "+rna_h5)
+	genes = data_loader(rna_h5, trainid)
+	genes.obs['lbs'] = lbs
+	gene_training_list.append(genes)
+	print("->Loading "+adt_h5)
+	proteins = data_loader(adt_h5, trainid)
+	proteins.obs['lbs'] = lbs
+	protein_training_list.append(proteins)
+
+print("----preparing test data..")
+rna_test_h5 = os.path.join(args.data_path, 'reference', 'rna'+args.impute_fids+'.h5')
+adt_test_h5 = os.path.join(args.data_path, 'gt', 'adt'+args.impute_fids+'.h5')
+lb_test_csv = os.path.join(args.data_path, 'reference', 'cty'+args.impute_fids+'.csv')
+lbs_test = pd.read_csv(lb_test_csv)['x'].tolist()
+print("->Loading "+rna_test_h5)
+gene_testing = data_loader(rna_test_h5, args.impute_fids)
+gene_testing.obs['lbs'] = lbs_test
+print("->Loading "+adt_test_h5)
+adt_testing = data_loader(adt_test_h5, args.impute_fids)
+adt_testing.obs['lbs'] = lbs_test
+
+print("---initializing sciPENN_API")
+sciPENN = sciPENN_API(gene_trainsets = gene_training_list, protein_trainsets = protein_training_list, gene_test = gene_testing, train_batchkeys = ['bid' for j in range(len(gene_training_list))], test_batchkey = 'bid')
+print("---training sciPENN")
+sciPENN.train(quantiles = [0.1, 0.25, 0.75, 0.9], n_epochs = 10000, ES_max = 12, decay_max = 6,
+             decay_step = 0.1, lr = 10**(-3), weights_dir = args.save_path, load = False)
+print("---conducting imputation")
+predicted_test = sciPENN.predict()
+end_time = time.time()
+all_time = end_time - begin_time
+print(all_time)
+np.savetxt(args.save_path+"/time.csv", [all_time], delimiter=",")
+
+
+print("---normalizing ground truth")
+_, protein_gt, _, _, _, _ = preprocess(gene_trainsets=[gene_testing], protein_trainsets=[adt_testing], train_batchkeys=['bid'], min_cells = 0, min_genes = 0)
+print(protein_gt.X)
+print(protein_gt.X.shape)
+print(protein_gt)
+
+print("---Saving data")
+file = h5py.File(args.save_path+"/imputed_result.h5",'w')
+file.create_dataset("prediction", data=predicted_test.X)
+file.create_dataset("groundtruth_raw", data= adt_testing.X)
+file.create_dataset("groundtruth_norm", data= protein_gt.X)
+
+
+
---
+++ tools_scripts/sciPENN/main_sciPENN_wolabel.py
@@ -0,0 +1,107 @@
+import os
+import h5py
+import time
+import random
+import argparse
+import numpy as np
+import scanpy as sc
+import pandas as pd
+from anndata import AnnData, read_h5ad
+from sciPENN.sciPENN_API import sciPENN_API
+from sciPENN.Preprocessing import preprocess
+
+random.seed(1)
+parser = argparse.ArgumentParser("sciPENN")
+parser.add_argument('--path1', metavar='DIR', nargs='+', default="", help='path to train data1')
+parser.add_argument('--path2', metavar='DIR', nargs='+', default="", help='path to train data2')
+parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
+args = parser.parse_args()
+
+begin_time = time.time()
+def load_data_vertical(rna_path,adt_path,i):
+  with h5py.File(adt_path, 'r') as f:
+    data_adt = np.array(f['matrix/data'])
+    barcodes_adt = np.array(f['matrix/barcodes'])
+    features_adt = np.array(f['matrix/features'])
+  with h5py.File(rna_path, 'r') as f:
+    data_rna = np.array(f['matrix/data'])
+    barcodes_rna = np.array(f['matrix/barcodes'])
+    features_rna = np.array(f['matrix/features'])
+    RNA_data = sc.AnnData(X=data_rna.T, obs=pd.DataFrame(index=barcodes_rna), var=pd.DataFrame(index=features_rna))
+    ADT_data = sc.AnnData(X=data_adt.T, obs=pd.DataFrame(index=barcodes_adt), var=pd.DataFrame(index=features_adt))
+    ADT_data.obs['batch'] = i
+    RNA_data.obs['batch'] = i
+    
+    print(RNA_data.obs)
+    return RNA_data, ADT_data
+    
+def generate_data(RNA_data, ADT_data):
+    adata_gene_train = RNA_data.copy()
+    adata_protein_train = ADT_data.copy()
+    
+    # Added by Sichang to make sure the type of data is correct for following codes.
+    for ad in [adata_gene_train, adata_protein_train]:
+        for key in ad.obs.columns:
+            if ad.obs[key].dtype == 'O':
+                ad.obs[key] = ad.obs[key].apply(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+
+        for key in ad.var.columns:
+            if ad.var[key].dtype == 'O':
+                ad.var[key] = ad.var[key].apply(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+    return adata_gene_train,adata_protein_train
+    
+def embedding_output(adata_gene_train, adata_protein_train):
+
+    for adata in adata_gene_train + adata_protein_train:
+        adata.obs.index = adata.obs.index.map(str)
+        
+    adata_gene_combined = adata_gene_train 
+    adata_protein_combined = adata_protein_train 
+    batch = []
+    for i in range(len(adata_gene_combined)):
+        adata_gene_combined[i].obs['batch'] = str(adata_gene_combined[i].obs['batch'])
+        adata_protein_combined[i].obs['batch'] = str(adata_protein_combined[i].obs['batch'])
+        batch.append('batch')
+        
+    sciPENN = sciPENN_API(gene_trainsets = adata_gene_combined, protein_trainsets = adata_protein_combined, train_batchkeys = batch, min_cells=0, min_genes=0) #min_cells=0, min_genes=0 is added by myself.
+    sciPENN.train(quantiles = [0.1, 0.25, 0.75, 0.9], n_epochs = 10000, ES_max = 12, decay_max = 6,
+             decay_step = 0.1, lr = 10**(-3), weights_dir = "pbmc_to_pbmc", load = False)
+    embedding_output = sciPENN.embed()
+    return embedding_output
+    
+
+    
+def whole_process(rna_paths,adt_paths):
+    adata_gene_train_list = []
+    adata_protein_train_list = []
+    i = 0
+    for rna_path, adt_path in zip(rna_paths, adt_paths):
+            RNA_data, ADT_data = load_data_vertical(rna_path,adt_path, i)
+            adata_gene_train,adata_protein_train = generate_data(RNA_data, ADT_data)
+            adata_gene_train_list.append(adata_gene_train)
+            adata_protein_train_list.append(adata_protein_train)
+            i = i + 1
+    standard_var_names = adata_gene_train_list[0].var_names
+
+    for adata in adata_gene_train_list:
+        adata.var_names = standard_var_names
+    embedding = embedding_output(adata_gene_train_list, adata_protein_train_list)
+    return embedding
+    
+# RUN METHOD
+result = whole_process(args.path1, args.path2)
+end_time = time.time()
+all_time = end_time - begin_time
+print(all_time)
+print(result.shape)
+
+# SAVE RESULTS
+if not os.path.exists(args.save_path):
+    os.makedirs(args.save_path)
+    print("create path")
+else:
+    print("the path exits")
+file = h5py.File(args.save_path+"/embedding.h5", 'w')
+file.create_dataset('data', data=result.X)
+file.close()
+np.savetxt(args.save_path+"/time.csv", [all_time], delimiter=",")
