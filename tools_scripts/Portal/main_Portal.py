ef362c610d6722b4c278e23472f729157e2aa750
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-12-20 10:44:11 +1100

    Update main_Portal.py

--- tools_scripts/Portal/main_Portal.py
+++ tools_scripts/Portal/main_Portal.py
@@ -1,35 +1,26 @@
 import os
 import h5py
+import time
+import portal
+import anndata
+import scipy.io
+import argparse
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import anndata
-import csv
-import gzip
-import scipy.io
-import portal
-import argparse
-import time
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-np.random.seed(1)
-
-sc.settings.verbosity = 3
-sc.logging.print_header()
-
-###
 parser = argparse.ArgumentParser("Portal")
-parser.add_argument('--path1', metavar='DIR', default='NULL', help='path to train data1')
-parser.add_argument('--path2', metavar='DIR', default='NULL', help='path to train data2')
-parser.add_argument('--cty_path1', metavar='DIR', default='NULL', help='path to train cty1')
-parser.add_argument('--cty_path2', metavar='DIR', default='NULL', help='path to train cty2')
+parser.add_argument('--path1', metavar='DIR', default='NULL', help='path to rna')
+parser.add_argument('--path2', metavar='DIR', default='NULL', help='path to atac')
 parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
 args = parser.parse_args()
-begin_time = time.time()
 
+# The Portal script for diagonal integration requires RNA and ATAC data as input, where ATAC needs to be transformed into gene activity score. The output is a joint embedding (dimensionality reduction).
+# run commond for Portal
+# python main_Portal.py --path1 "../../data/dataset_final/D27/rna.h5" --path2 "../../data/dataset_final/D27/atac_gas.h5" --save_path "../../result/embedding/diagonal integration/D27/Portal/"
+
+begin_time = time.time()
 rna_h5_files = args.path1
-rna_label_files = args.cty_path1
 h5 = h5py.File(rna_h5_files, "r")
 h5_rna_data = h5['matrix/data']
 h5_rna_barcodes = h5['matrix/barcodes']
@@ -37,15 +28,12 @@ h5_rna_features = h5['matrix/features']
 rna_data = scipy.sparse.csr_matrix(np.array(h5_rna_data).transpose()).copy()
 rna_barcodes = np.array(h5_rna_barcodes).astype(str).copy()
 rna_features = np.array(h5_rna_features).astype(str).copy()
-rna_label = pd.read_csv(rna_label_files, index_col = 0)
 adata_rna = anndata.AnnData(rna_data)
 adata_rna.obs.index = rna_barcodes
-adata_rna.obs["cell_type"] = rna_label["x"].values.astype(str)
 adata_rna.obs["data_type"] = "rna"
 adata_rna.var.index = rna_features
 
 atac_h5_files = args.path2
-atac_label_files = args.cty_path2
 h5 = h5py.File(atac_h5_files, "r")
 h5_atac_data = h5['matrix/data']
 h5_atac_barcodes = h5['matrix/barcodes']
@@ -53,10 +41,8 @@ h5_atac_features = h5['matrix/features']
 atac_data = scipy.sparse.csr_matrix(np.array(h5_atac_data).transpose()).copy()
 atac_barcodes = np.array(h5_atac_barcodes).astype(str).copy()
 atac_features = np.array(h5_atac_features).astype(str).copy()
-atac_label = pd.read_csv(atac_label_files, index_col = 0)
 adata_atac = anndata.AnnData(atac_data)
 adata_atac.obs.index = atac_barcodes
-adata_atac.obs["cell_type"] = atac_label["x"].values.astype(str)
 adata_atac.obs["data_type"] = "atac"
 adata_atac.var.index = atac_features
 
