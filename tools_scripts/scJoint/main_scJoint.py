889bdb0990e59aec70b1a8397dc151faa0c15851
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-12-20 20:10:40 +1100

    Update main_scJoint.py

--- tools_scripts/scJoint/main_scJoint.py
+++ tools_scripts/scJoint/main_scJoint.py
@@ -1,24 +1,18 @@
+import os
 import h5py
-import os
-import argparse
-import numpy as np
 import time
-import pandas as pd
 import torch
-import os
-from datetime import datetime
-
-from config import Config
-from util.trainingprocess_stage1 import TrainingProcessStage1
-from util.trainingprocess_stage3 import TrainingProcessStage3
-from util.knn import KNN
 import parser
 import argparse
-import h5py
 import process_db
+import numpy as np
 import pandas as pd
+from util.knn import KNN
+from config import Config
+from datetime import datetime
+from util.trainingprocess_stage1 import TrainingProcessStage1
+from util.trainingprocess_stage3 import TrainingProcessStage3
 
-###
 ### this code is from https://github.com/rsinghlab/SCOT
 begin_time = time.time()
 
@@ -27,8 +21,8 @@ parser.add_argument('--threads', type=int, default=1, help='threads')
 parser.add_argument('--use_cuda', type=bool, default= True, help='if use gpu or not')
 parser.add_argument('--with_crossentorpy', type=bool, default= True, help='if use _crossentorpy or not')
 
-parser.add_argument('--path1', metavar='DIR', nargs='*', default=[], help='path to data1')
-parser.add_argument('--path2', metavar='DIR', nargs='*', default=[], help='path to data2')
+parser.add_argument('--path1', metavar='DIR', nargs='*', default=[], help='path to rna')
+parser.add_argument('--path2', metavar='DIR', nargs='*', default=[], help='path to atac')
 parser.add_argument('--cty_path1', metavar='DIR', nargs='*', default=[], help='path to cty1')
 parser.add_argument('--cty_path2', metavar='DIR', nargs='*', default=[], help='path to cty2')
 parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
@@ -51,6 +45,10 @@ parser.add_argument('--embedding_size', type=int, default=64, help='batch size')
 parser.add_argument('--center_weight', type=int, default=1, help='center_weight')
 parser.add_argument('--seed', type=int, default=1, help='seed')
 args = parser.parse_args()
+
+# The scJoint script for diagonal integration requires RNA and ATAC data as input, also with the label of RNA and ATAC (optional), where ATAC needs to be transformed into gene activity score. The output is a joint embedding (dimensionality reduction).
+# run commond for scJoint
+# python main_scJoint.py --path1 "../../data/dataset_final/D27/rna.h5" --path2 "../../data/dataset_final/D27/atac_gas.h5" --cty_path1 "../../data/dataset_final/D27/rna_cty.csv" --cty_path2 "../../data/dataset_final/D27/atac_cty.csv" --save_path "../../result/embedding/diagonal integration/D27/scJoint/"
 
 if not args.use_cuda:
     args.device = torch.device('cpu')
@@ -75,13 +73,15 @@ def main():
     h5_data = h5['matrix/data']
     process_db.data_parsing(rna_h5_files, atac_h5_files)
     
-    rna_label = pd.read_csv(rna_label_files[0], index_col = 0)
-    rna_label
-    print(rna_label.value_counts(sort = False))
+    rna_label = pd.read_csv(rna_label_files[0])#, index_col = 0)
+    rna_label.index = range(1, len(rna_label) + 1)
+    print(rna_label, "!!")
+#    print(rna_label.value_counts(sort = False))
     if (atac_label_files != []):
-        atac_label = pd.read_csv(atac_label_files[0], index_col = 0)
-        atac_label
-        print(atac_label.value_counts(sort = False))
+        atac_label = pd.read_csv(atac_label_files[0])#, index_col = 0)
+        atac_label.index = range(1, len(atac_label) + 1)
+        print(atac_label)
+    #    print(atac_label.value_counts(sort = False))
         atac_label_files_temp = atac_label_files
     else:
         atac_label = []
@@ -90,6 +90,7 @@ def main():
     feature_num = h5_data.shape[0]
     num_cty = 50 #rna_label['x'].nunique()
     
+    print(rna_label_files, atac_label_files_temp, "!!!!")
     process_db.label_parsing(rna_label_files, atac_label_files_temp)
 
     args.rna_paths = [file.replace(".h5", ".npz") for file in rna_h5_files]
@@ -126,20 +127,20 @@ def main():
     print('KNN finished: ', datetime.now().strftime('%H:%M:%S'))
     
     # stage3 training
-    print('Training start [Stage3]')
-    model_stage3 = TrainingProcessStage3(args)    
-    for epoch in range(args.epochs_stage3):
-       print('Epoch:', epoch)
-       model_stage3.train(epoch)
+    #print('Training start [Stage3]')
+    #model_stage3 = TrainingProcessStage3(args)
+    #for epoch in range(args.epochs_stage3):
+    #   print('Epoch:', epoch)
+    #   model_stage3.train(epoch)
         
-    print('Write embeddings [Stage3]')
-    model_stage3.write_embeddings()
-    print('Stage 3 finished: ', datetime.now().strftime('%H:%M:%S'))
+    #print('Write embeddings [Stage3]')
+    #model_stage3.write_embeddings()
+    #print('Stage 3 finished: ', datetime.now().strftime('%H:%M:%S'))
     
     # KNN
-    print('KNN stage3')
-    KNN(args, neighbors = 30, knn_rna_samples=20000)
-    print('KNN finished: ', datetime.now().strftime('%H:%M:%S'))
+    #print('KNN stage3')
+    #KNN(args, neighbors = 30, knn_rna_samples=20000)
+    #print('KNN finished: ', datetime.now().strftime('%H:%M:%S'))
     
     end_time = time.time()
     all_time = end_time - begin_time
@@ -177,6 +178,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
-    
