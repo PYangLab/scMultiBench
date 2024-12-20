86b4240626c677b8e19410ec1ab38c31b68f3db0
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-12-20 14:16:37 +1100

    Update main_MultiMAP.py

--- tools_scripts/MultiMAP/main_MultiMAP.py
+++ tools_scripts/MultiMAP/main_MultiMAP.py
@@ -1,13 +1,13 @@
 import os
-import argparse
-import scanpy as sc
-import anndata
-import MultiMAP
+import time
 import h5py
+import anndata
+import argparse
+import MultiMAP
 import numpy as np
+import scanpy as sc
 from anndata import AnnData
 from scipy.sparse import csr_matrix
-import time
 
 begin_time = time.time()
 parser = argparse.ArgumentParser("MultiMAP")
@@ -17,7 +17,11 @@ parser.add_argument('--path3', metavar='DIR', default='NULL', help='path to trai
 parser.add_argument('--save_path', metavar='DIR', default='NULL', help='path to save the output data')
 args = parser.parse_args()
 
-def runMultiMAP(rna_path, atac_peaks_path, atac_genes_path): 
+# The MultiMAP script for diagonal integration requires RNA and ATAC data as input, where ATAC needs to be transformed into gene activity score. The output is a joint embedding (dimensionality reduction).
+# run commond for MultiMAP
+# python main_MultiMAP.py --path1 "../../data/dataset_final/D27/rna.h5" --path2 "../../data/dataset_final/D27/atac_peak.h5" --path3 "../../data/dataset_final/D27/atac_gas.h5" --save_path "../../result/embedding/diagonal integration/D27/MultiMAP/"
+
+def runMultiMAP(rna_path, atac_peaks_path, atac_genes_path):
     with h5py.File(rna_path, "r") as f:
             X = np.mat(np.array(f['matrix/data']).transpose())
             rna = AnnData(X=X)
@@ -33,9 +37,6 @@ def runMultiMAP(rna_path, atac_peaks_path, atac_genes_path):
             atac_genes = AnnData(X=X)
             atac_genes.X = csr_matrix(np.matrix(atac_genes.X))
             
-    print(rna.X.shape)
-    print(atac_peaks.X.shape)
-    print(atac_genes.X.shape)
     MultiMAP.TFIDF_LSI(atac_peaks)
     atac_genes.obsm['X_lsi'] = atac_peaks.obsm['X_lsi'].copy()
 
@@ -48,21 +49,16 @@ def runMultiMAP(rna_path, atac_peaks_path, atac_genes_path):
     result = adata.obsm['X_multimap']
     return result
 
-
 result = runMultiMAP(args.path1, args.path2, args.path3) # 2is peak, 3 is gas
-
 end_time = time.time()
 all_time = end_time - begin_time
 print(all_time)
-
 
 if not os.path.exists(args.save_path):
     os.makedirs(args.save_path)
     print("create path")
 else:
     print("the path exits")
-
-
 file = h5py.File(args.save_path+"/embedding.h5", 'w')
 file.create_dataset('data', data=result)
 file.close()
